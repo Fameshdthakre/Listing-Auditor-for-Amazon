fd09e31742342459719e56ed569e6fa6b19fbe94
TransFamesh <fameshdthakre@yahoo.com>
2026-01-21 21:45:42 +0530

    Delete config.template.js

--- config.template.js
+++
@@ -1,6 +0,0 @@
-// Auto-generated config file. Do not edit directly.
-// Edit config.template.js and run the build script instead.
-
-export const MS_CLIENT_ID = "{{MS_CLIENT_ID}}";
-export const MS_AUTH_URL = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize";
-export const MS_SCOPES = "openid profile User.Read email Files.ReadWrite";
